43eef31e3f6a84fb88e0b79bb8996e70c2e1cf16
Shane Linden <shanelinden1995@gmail.com>
2021-04-16 09:23:05 +0200

    Adds in functionality

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,10 +1,37 @@
+import { JwtManager } from './../utils/jwt-manager'
+import { PasswordManager } from '../utils/password-manager'
 import { Router, Request, Response } from 'express'
-import { body, Result, validationResult, ValidationError } from 'express-validator'
+import { body } from 'express-validator'
+import { BadRequestError } from '../errors/bad-request-error'
+import { validateRequest } from '../middleware/validate-request'
+import { User } from '../models/user'
 
 const router = Router()
 
-router.post('/api/users/signIn', (req: Request, res: Response) => {
-  res.send({})
+const signInValidation = [
+  body('email').isEmail().withMessage('Email must be valid.'),
+  body('password').trim().notEmpty().withMessage('You must enter in a password.'),
+]
+
+router.post('/api/users/signIn', signInValidation, validateRequest, async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  const existingUser = await User.findOne({ email })
+  if (!existingUser) {
+    throw new BadRequestError('Invalid credentials')
+  }
+
+  const passwordsMatch = await PasswordManager.compare(existingUser.password, password)
+
+  if (!passwordsMatch) {
+    throw new BadRequestError('Invalid credentials')
+  }
+
+  req.session = {
+    jwt: JwtManager.signUserJwt(existingUser),
+  }
+
+  res.status(200).send(existingUser)
 })
 
 export { router as signInRouter }
