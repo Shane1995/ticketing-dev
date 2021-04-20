d87cb0b38181c05f6d70db8d6cb0c9501b132d98
Shane Linden <shanelinden1995@gmail.com>
2021-04-20 07:53:16 +0200

    Adds implementation for fetching the current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from 'express'
+import { currentUser } from '../middleware/current-user'
 
 const router = Router()
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-  res.send('hi')
+router.get('/api/users/currentuser', currentUser, (req: Request, res: Response) => {
+  res.send({ currentUser: req.currentUser || null })
 })
 
 export { router as currentUserRouter }
