b0902f31c058e311e0e86e3b19117695e04f77b8
Shane Linden <shanelinden1995@gmail.com>
2021-04-20 07:53:51 +0200

    Adds implementation for signing out a user

--- auth/src/routes/sign-out.ts
+++ auth/src/routes/sign-out.ts
@@ -3,7 +3,8 @@ import { Router, Request, Response } from 'express'
 const router = Router()
 
 router.post('/api/users/signOut', (req: Request, res: Response) => {
-  res.send('hi')
+  req.session = null
+  res.send({})
 })
 
 export { router as signOutRouter }
