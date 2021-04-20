f0792dbdf11e5c2f59be3d52a9bd5efc92da2825
Shane Linden <shanelinden1995@gmail.com>
2021-04-20 07:52:25 +0200

    Refactor for supertest

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express'
+import 'express-async-errors'
+
+import cookieSession from 'cookie-session'
+
+import { currentUserRouter } from './routes/current-user'
+import { signInRouter } from './routes/sign-in'
+import { signOutRouter } from './routes/sign-out'
+import { signUpRouter } from './routes/sign-up'
+import { errorHandler } from './middleware/error-handler'
+import { NotFoundError } from './errors/not-found-error'
+
+const app = express()
+
+// Ensure express is aware of being behind nginx.
+app.set('trust proxy', true)
+app.use(express.json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+)
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
+
+app.all('*', () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from 'express'
-import 'express-async-errors'
+import { app } from './app'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-
-import { currentUserRouter } from './routes/current-user'
-import { signInRouter } from './routes/sign-in'
-import { signOutRouter } from './routes/sign-out'
-import { signUpRouter } from './routes/sign-up'
-import { errorHandler } from './middleware/error-handler'
-import { NotFoundError } from './errors/not-found-error'
-
-const app = express()
-
-// Ensure express is aware of being behind nginx.
-app.set('trust proxy', true)
-app.use(express.json())
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-)
-app.use(currentUserRouter)
-app.use(signInRouter)
-app.use(signOutRouter)
-app.use(signUpRouter)
-
-app.all('*', () => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
