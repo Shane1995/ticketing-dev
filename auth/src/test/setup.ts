50fb4d3185812dd5bbd82e2b347fbaf04db1c1a3
Shane Linden <shanelinden1995@gmail.com>
2021-04-21 08:46:10 +0200

    Adds a global helper method for retrieving a cookie

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
+import request from 'supertest'
 import { app } from '../app'
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      getAuthCookie(): Promise<string[]>
+    }
+  }
+}
 
 let mongo: MongoMemoryServer
 
@@ -32,3 +41,12 @@ afterAll(async () => {
   await mongo.stop()
   await mongoose.connection.close()
 })
+
+global.getAuthCookie = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const response = await request(app).post('/api/users/signup').send({ email, password }).expect(201)
+  const cookie = response.get('Set-Cookie')
+  return cookie
+}
