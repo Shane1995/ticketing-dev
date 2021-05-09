c0f109c4282828ab8b38d0ff819a377dcbe26cb1
Shane Linden <Shane.Linden@derivco.co.za>
2021-05-09 12:33:42 +0200

    adds user sign-up

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,14 @@
-const Index = () => {
-  return (
-    <>
-      <header>
-        <h1>DerivCare</h1>
-        <ul>
-          <li>Home</li>
-          <li>Support</li>
-          <li>Contact</li>
-        </ul>
-      </header>
-    </>
-  )
+import axios from 'axios'
+
+const landingPage = ({ currentUser }) => {
+  console.log(currentUser)
+  return <h1 style={{ color: color }}>Landing Page</h1>
 }
 
-export default Index
+landingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser')
+
+  return response.data
+}
+
+export default landingPage
